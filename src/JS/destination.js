22c74dd27e13891c30d80b2a77441ebce886656e
somyagupta1606-netizen <somyagupta1606@gmail.com>
2025-12-19 10:05:21 +0530

    day3 done

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -1,12 +1,45 @@
 const params = new URLSearchParams(window.location.search);
 
-const city =params.get("city");
+const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
 
-if(city){
-    cityName.textContent = city;
-}
-else{
-    cityName.textContent ="Unknown city";
+let GEODB_API_KEY = "4d6d85ff85mshd0e2e3f489344e7p104d3cjsn5e39278759b0";
+let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
+
+if (city) {
+  cityName.textContent = city;
+
+  async function fetchCityDetails(cityName) {
+    try {
+      let result = await fetch(
+        `https://${GEODB_HOST}/v1/geo/cities?namePrefix=${city}&limit=1`,
+        {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key": GEODB_API_KEY,
+            "X-RapidAPI-HOST": GEODB_HOST,
+          },
+        }
+      );
+      console.log(result);
+      if(!result.ok){
+        console.log("API Response Not Found..........");
+        
+      }
+      let data = await result.json();
+      console.log(data);
+      if(!data.data||data.data.length===0){
+        console.log("city not found");
+        
+      }
+      let cityData =data.data[0];
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  fetchCityDetails(city);
+} else {
+  cityName.textContent = "Unknown city";
 }
