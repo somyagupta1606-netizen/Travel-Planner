5023d00cd0a37973926664184202e9807e8f9e8f
somyagupta1606-netizen <somyagupta1606@gmail.com>
2025-12-23 10:10:31 +0530

    day 4 done

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -3,6 +3,16 @@ const params = new URLSearchParams(window.location.search);
 const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
+const loading =document.querySelector("#loading");
+const cityInfoBox =document.querySelector("#cityInfo")
+const errorBox =document.querySelector("#error")
+
+const lat = document.querySelector("#lat")
+const lon = document.querySelector("#lon")
+const country =document.querySelector("#country")
+const state =document.querySelector("#state")
+const population =document.querySelector("#population")
+
 
 let GEODB_API_KEY = "4d6d85ff85mshd0e2e3f489344e7p104d3cjsn5e39278759b0";
 let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
@@ -34,12 +44,26 @@ if (city) {
         
       }
       let cityData =data.data[0];
+      console.log(cityData);
+
+      lat.textContent =cityData.latitude;
+      lon.textContent =cityData.longitude;
+      country.textContent=cityData.country;
+      state.textContent=cityData.region||"N/A";
+      population.textContent=cityData.population?cityData.population.toLocaleString():"N/A"
+
+      loading.classList.add("hidden");
+      cityInfoBox.classList.remove("hidden");
+      
       
     } catch (error) {
       console.log(error);
+         loading.classList.add("hidden");
+      errorBox.classList.remove("hidden");
     }
   }
   fetchCityDetails(city);
 } else {
   cityName.textContent = "Unknown city";
+  loading.classList.add("hidden");
 }
